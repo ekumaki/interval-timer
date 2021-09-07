c7a7c214ad651f8916f61fe7cc990dfc0410f4d5
ekumaki <jetblack13k13@gmail.com>
2021-09-07 12:15:33 +0900

    ✨ stopボタンとresetボタンの追加

--- src/compornents/Main/index.jsx
+++ src/compornents/Main/index.jsx
@@ -1,37 +1,54 @@
 // import Image from 'next/image'
+import { useState } from "react";
 import { Headline } from "src/compornents/Headline";
 import styles from "src/compornents/Main/Main.module.css";
 
 export function Main() {
+  const [timer, setTimer] = useState("00:00");
+  const [timeoutId, setTimeoutId] = useState();
   let startTime;
+  // let timeoutId;
 
   const countUp = () => {
-    console.log(Date.now() - startTime); //スタートボタンを押した時間との差
-    // const d = new Date(Date.now() - startTime);
-    // const m = String(d.getMinutes()).padStart(2, "0");
-    // const s = String(d.getSeconds()).padStart(2, "0");
-    // const ms = String(d.getMinutes()).padStart(2, "0");
-    // timer.textContent = `${m}:${s}:${ms}`;
+    // console.log(Date.now() - startTime);
+    //スタートボタンを押した時間との差
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, "0");
+    const s = String(d.getSeconds()).padStart(2, "0");
+    const ms = String(d.getMilliseconds()).padStart(2, "0");
+    setTimer(`${m}:${s}:${ms}`);
 
-    setTimeout(() => {
-      countUp();
-    }, 100);
+    // ストップ用にsetTimeoutの返り値を取得
+    setTimeoutId(
+      setTimeout(() => {
+        countUp();
+      }, 10)
+    );
   };
 
+  // スタートボタンの処理
   const handleStart = () => {
     startTime = Date.now(); // スタートボタンを押した時刻を取得
     countUp();
   };
 
+  // ストップボタンの処理
+  const handleStop = () => {
+    clearTimeout(timeoutId);
+  };
+
+  // リセットボタンの処理
+  const handleReset = () => {
+    setTimer("00:00");
+  };
+
   return (
     <main className={styles.main}>
       <Headline />
-      <div id="timer">00:00</div>
-      <button id="start" onClick={handleStart}>
-        Start
-      </button>
-      <button id="stop">Stop</button>
-      <button id="reset">Reset</button>
+      <div>{timer}</div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
     </main>
   );
 }
