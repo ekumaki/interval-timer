a7768a68695f13f9e20548b1cec2dbda33e75e9b
ekumaki <jetblack13k13@gmail.com>
2021-09-10 23:39:03 +0900

    ✨ カウントアップ機能の作成

--- src/compornents/Main/index.jsx
+++ src/compornents/Main/index.jsx
@@ -4,43 +4,57 @@ import { Headline } from "src/compornents/Headline";
 import styles from "src/compornents/Main/Main.module.css";
 
 export function Main() {
-  const [timer, setTimer] = useState("00:00");
+  // const [startTime, setStartTime] = useState(0);
+  const [timer, setTimer] = useState(0);
   const [timeoutId, setTimeoutId] = useState();
-  let startTime;
-  // let timeoutId;
+  // const [elapsedTime, setElapsedTime] = useState(0); // 経過時間
+  // let startTime;
+  // let elapsedTime = 0;
 
-  const countUp = () => {
-    // console.log(Date.now() - startTime);
-    //スタートボタンを押した時間との差
-    const d = new Date(Date.now() - startTime);
-    const m = String(d.getMinutes()).padStart(2, "0");
-    const s = String(d.getSeconds()).padStart(2, "0");
-    const ms = String(d.getMilliseconds()).padStart(2, "0");
-    setTimer(`${m}:${s}:${ms}`);
+  // const countUp = () => {
+  //   //スタートボタンを押した時間との差
+  //   // const d = new Date(Date.now() - startTime + elapsedTime);
+  //   // const d = new Date(Date.now() - startTime);
+  //   // const m = String(d.getMinutes()).padStart(2, "0");
+  //   // const s = String(d.getSeconds()).padStart(2, "0");
+  //   // const ms = String(d.getMilliseconds()).padStart(2, "0");
+  //   // setTimer(`${m}:${s}:${ms}`);
 
-    // ストップ用にsetTimeoutの返り値を取得
-    setTimeoutId(
-      setTimeout(() => {
-        countUp();
-      }, 10)
-    );
-  };
+  //   setTimer(startTime);
 
-  // スタートボタンの処理
+  //   // ストップ用にsetTimeoutの返り値を取得
+  //   setTimeoutId(
+  //     setTimeout(() => {
+  //       countUp();
+  //     }, 10)
+  //   );
+  // };
+
   const handleStart = () => {
-    startTime = Date.now(); // スタートボタンを押した時刻を取得
-    countUp();
+    setTimeoutId(
+      setInterval(() => {
+        setTimer((prevTimer) => {
+          prevTimer++;
+          return prevTimer;
+        });
+      }, 1000)
+    );
   };
 
   // ストップボタンの処理
   const handleStop = () => {
     clearTimeout(timeoutId);
+    // elapsedTime = Date.now() - startTime;
+    // setElapsedTime(Date.now() - startTime);
   };
 
   // リセットボタンの処理
   const handleReset = () => {
-    setTimer("00:00");
+    setTimer(0);
   };
+
+  // console.log(elapsedTime);
+  // console.log({newStartTime});
 
   return (
     <main className={styles.main}>
