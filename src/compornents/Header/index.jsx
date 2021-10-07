b323f93cedb35a2ccb353920aa709fdb530c75b8
ekumaki <jetblack13k13@gmail.com>
2021-10-07 22:09:06 +0900

    ✨ ヘッダーの作成とヘッドライの削除

---
+++ src/compornents/Header/index.jsx
@@ -0,0 +1,10 @@
+// import Image from 'next/image'
+import styles from "src/compornents/Header/Header.module.css";
+
+export function Header() {
+  return (
+    <div>
+      <h1 className={styles.title}>Interval Timer</h1>
+    </div>
+  );
+}
