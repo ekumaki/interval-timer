72f7c99f76cda74d2253425c21b23a5d7c0d5292
ekumaki <jetblack13k13@gmail.com>
2021-11-24 22:34:09 +0900

    ♻️ footerの加工

--- src/compornents/Footer/index.jsx
+++ src/compornents/Footer/index.jsx
@@ -3,13 +3,7 @@ import styles from "src/compornents/Footer/Footer.module.css";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        exMachina
-      </a>
+      <p>&copy; 2021 exMachina</p>
     </footer>
   );
 }
